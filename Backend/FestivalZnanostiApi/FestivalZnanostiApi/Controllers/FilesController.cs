20c84870f3583447c6478137409df281a79951c0
matej352 <matej.durin1@gmail.com>
2023-10-26 23:29:06 +0200

    create festival table staticly

--- Backend/FestivalZnanostiApi/FestivalZnanostiApi/Controllers/FilesController.cs
+++ Backend/FestivalZnanostiApi/FestivalZnanostiApi/Controllers/FilesController.cs
@@ -23,6 +23,7 @@ namespace FestivalZnanostiApi.Controllers
 
 
         [HttpGet]
+        [Route("EventSummary")]
         public IActionResult GenerateEventSummary(int EventId)
         {
             var document = _filesService.GenerateEventSummary(EventId);
@@ -31,6 +32,29 @@ namespace FestivalZnanostiApi.Controllers
             var contentDisposition = new ContentDisposition
             {
                 FileName = $"EventSummary_{EventId}.pdf",
+                Inline = false,  // Set to false to force download
+            };
+
+            Response.Headers.Add("Content-Disposition", contentDisposition.ToString());
+
+
+
+            return File(document, "application/pdf");
+
+
+        }
+
+
+        [HttpGet]
+        [Route("FestivalTable")]
+        public IActionResult GenerateFestivalTable(int FestivalYear)
+        {
+            var document = _filesService.GenerateFestivalTable(FestivalYear);
+
+            // Set the Content-Disposition header to force download with a specific file name
+            var contentDisposition = new ContentDisposition
+            {
+                FileName = $"FestivalTable_{FestivalYear}.pdf",
                 Inline = false,  // Set to false to force download
             };
 
--- Backend/FestivalZnanostiApi/FestivalZnanostiApi/Services/IFilesService.cs
+++ Backend/FestivalZnanostiApi/FestivalZnanostiApi/Services/IFilesService.cs
@@ -1,8 +1,12 @@
-﻿namespace FestivalZnanostiApi.Services
+﻿using FestivalZnanostiApi.Models;
+
+namespace FestivalZnanostiApi.Services
 {
     public interface IFilesService
     {
 
         public byte[] GenerateEventSummary(int EventId);
+
+        public byte[] GenerateFestivalTable(int FestivalYear);
     }
 }
